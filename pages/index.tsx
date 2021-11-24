04bb7f66d32e6dc1ac13e2dcf28470b81c8b0495
Antonio Lo Fiego <alofiego@hey.com>
2021-11-24 18:11:13 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+	return (
+		<>
+			<Head>
+				<title>learntocloud.guide</title>
+				<meta
+					name='description'
+					content='Generated with the Batteries Included Next App template'
+				/>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>
+				<h1>learntocloud.guide</h1>
+			</main>
+		</>
+	);
+};
+
+export default Home;
