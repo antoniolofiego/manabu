ddef38deafd4681d6649b3039d405f6af9541c8d
Antonio Lo Fiego <alofiego@hey.com>
2021-11-24 22:44:36 -0500

    Assemble first portion of the Dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+
+import Phase from '@components/Phase';
+import SearchBar from '@components/SearchBar';
+
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
@@ -15,9 +19,44 @@ const Dashboard: React.FC = () => {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<h1>Dashboard</h1>
-			<div>
-				<h2>Test</h2>
+			<div className='grid items-center px-12 mx-auto space-y-16 max-w-7xl'>
+				<SearchBar />
+				<h2 className='text-2xl font-extrabold justify-self-center lg:justify-self-start'>
+					Hello, Katherine
+				</h2>
+				<div className='grid w-full gap-12 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:grid-rows-2'>
+					<Phase
+						number={1}
+						name='Linux & networking'
+						stage='3. Introduction to Networking'
+						progress={0.5}
+						url='intro-to-networking'
+					/>
+					<Phase
+						number={2}
+						name='Programming & Scripting'
+						stage='2. Automation with Python'
+						progress={0.33}
+						url='automation-with-python'
+					/>
+					<Phase
+						number={3}
+						name='Cloud Platforms'
+						stage='1. Cloud Service Providers'
+						progress={0.0}
+						url='cloud-service-providers'
+					/>
+					<Phase
+						number={4}
+						name='DevOps Fundamentals'
+						stage='1. DevOps concepts'
+						progress={0.0}
+						url='devops-concepts'
+					/>
+				</div>
+				<h2 className='text-2xl font-extrabold'>
+					Looking for something to do?
+				</h2>
 			</div>
 		</>
 	);
