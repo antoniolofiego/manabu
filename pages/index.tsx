9c36cd4e55b4914e286b115254232577acf01620
Antonio Lo Fiego <alofiego@hey.com>
2021-11-24 20:08:36 -0500

    Create shell of unauthorized index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 const Home: React.FC = () => {
+	const [loggedIn, setLoggedIn] = useState(false);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (loggedIn) {
+			router.push('/dashboard');
+		}
+	}, [loggedIn, router]);
+
 	return (
 		<>
 			<Head>
@@ -11,8 +22,12 @@ const Home: React.FC = () => {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main>
-				<h1>learntocloud.guide</h1>
+			<main className='flex-1 min-w-0 px-2 overflow-auto bg-indigo-50'>
+				{!loggedIn && (
+					<div>
+						<button onClick={() => setLoggedIn(true)}>logIn</button>
+					</div>
+				)}
 			</main>
 		</>
 	);
