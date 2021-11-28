d7b39ed67ed95d6dfefae61a90efda713dfceffd
Antonio Lo Fiego <alofiego@hey.com>
2021-11-27 19:55:08 -0500

    Change position of Search icon in Search bar

--- components/shared/SearchBar/index.tsx
+++ components/shared/SearchBar/index.tsx
@@ -6,6 +6,7 @@ const SearchBar: React.FC = () => {
 
 	return (
 		<div className='flex items-center w-full max-w-2xl mx-auto my-8 border border-black rounded-2xl group focus-within:ring-2 dark:border-gray-50'>
+			<SearchIcon className='h-4 pl-4' />
 			<input
 				className='w-full px-4 py-2 mx-auto transition-all focus:outline-none dark:bg-gray-900 rounded-2xl dark:placeholder-gray-200'
 				placeholder='Search'
@@ -14,7 +15,6 @@ const SearchBar: React.FC = () => {
 					setSearchTerms(e.currentTarget.value)
 				}
 			/>
-			<SearchIcon className='h-4 px-4' />
 		</div>
 	);
 };
