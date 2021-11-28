4a61c2ce7972d036ca87e05cf372444a076d5266
Antonio Lo Fiego <alofiego@hey.com>
2021-11-27 21:41:02 -0500

    Offload dark styles to outer div in layout component

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,11 +4,9 @@ type Props = { children: JSX.Element };
 
 const Layout: React.FC<Props> = ({ children }) => {
 	return (
-		<div className='flex min-h-screen'>
+		<div className='flex min-h-screen transition-all dark:bg-gray-900 dark:text-gray-50'>
 			<Sidebar />
-			<div className='flex-1 min-w-0 overflow-auto transition bg-indigo dark:bg-gray-900 dark:text-gray-50'>
-				{children}
-			</div>
+			<div className='flex-1 min-w-0 overflow-auto'>{children}</div>
 		</div>
 	);
 };
