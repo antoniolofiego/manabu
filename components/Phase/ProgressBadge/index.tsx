dccb9aeb6e404acaea62b1fc3722b73291aa1b96
Antonio Lo Fiego <alofiego@hey.com>
2021-11-27 19:29:18 -0500

    Make progress bar reactive to theme changes

--- components/Phase/ProgressBadge/index.tsx
+++ components/Phase/ProgressBadge/index.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from 'next-themes';
+
 type ProgressBadgeProps = {
 	percent: number;
 	width?: number;
@@ -11,25 +13,36 @@ const ProgressBadge: React.FC<ProgressBadgeProps> = ({
 	strokeWidth = 1,
 	circleColor,
 }) => {
+	const { resolvedTheme } = useTheme();
+
+	const circleFill = resolvedTheme === 'light' ? 'text-white' : 'text-gray-900';
+
+	const strokeFill =
+		resolvedTheme === 'light' ? 'black' : 'rgba(249, 250, 251, 100)';
+
 	const MIN_OFFSET = 300; // Ensuring that even when it's 0 there's a minimum portion of circle
 
-	const pos = width / 2;
+	const pos = width / 2; // Always centered in the SVG viewbox
 	const radius = pos - strokeWidth * 2;
 	const circumference = radius * 2 * Math.PI;
 
 	const offset = Math.min(circumference - percent * circumference, MIN_OFFSET);
 
+	console.table([resolvedTheme, circleFill]);
+
 	return (
 		<span className='relative flex items-center justify-center flex-shrink'>
+			<span className='z-10 text-2xl'>{percent * 100}%</span>
 			<svg
 				height={width}
 				width={width}
 				className='absolute transform -rotate-90'
 			>
 				<circle
+					className={circleFill}
 					strokeWidth={strokeWidth}
-					stroke='currentColor'
-					fill='transparent'
+					stroke={strokeFill}
+					fill='currentColor'
 					r={radius}
 					cx={pos}
 					cy={pos}
@@ -47,7 +60,6 @@ const ProgressBadge: React.FC<ProgressBadgeProps> = ({
 					cy={pos}
 				/>
 			</svg>
-			<span className='text-2xl'>{percent * 100}%</span>
 		</span>
 	);
 };
--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -5,16 +5,16 @@ const SearchBar: React.FC = () => {
 	const [searchTerms, setSearchTerms] = useState('');
 
 	return (
-		<div className='flex items-center w-full max-w-2xl px-4 py-2 mx-auto my-8 space-x-4 border border-black rounded-2xl group focus-within:ring-2'>
-			<SearchIcon className='h-4' />
+		<div className='flex items-center w-full max-w-2xl my-8 border border-black rounded-2xl group focus-within:ring-2 dark:border-gray-50'>
 			<input
-				className='focus:outline-none'
+				className='w-full px-4 py-2 mx-auto transition-all focus:outline-none dark:bg-gray-900 rounded-2xl dark:placeholder-gray-200'
 				placeholder='Search'
 				value={searchTerms}
 				onChange={(e: React.FormEvent<HTMLInputElement>) =>
 					setSearchTerms(e.currentTarget.value)
 				}
 			/>
+			<SearchIcon className='h-4 px-4' />
 		</div>
 	);
 };
