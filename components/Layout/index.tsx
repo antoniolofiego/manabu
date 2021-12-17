84b25bfa490d82638a5d02e7b4cac0f1eee35935
Antonio Lo Fiego <alofiego@hey.com>
2021-12-17 18:54:00 -0500

    Move ThemeSwitcherButton to layout instead of profile menu

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,16 @@
 import Sidebar from './Sidebar';
+import ThemeSwitcherButton from '@components/shared/ThemeSwitcherButton';
 
-type Props = { children: JSX.Element };
+type LayoutProps = { children: JSX.Element };
 
-const Layout: React.FC<Props> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
-		<div className='flex min-h-screen transition-all bg-gray-50 dark:bg-gray-900 dark:text-gray-50'>
+		<div className='relative flex min-h-screen transition-all bg-gray-50 dark:bg-gray-900 dark:text-gray-50'>
 			<Sidebar />
 			<div className='flex-1 min-w-0 overflow-auto'>{children}</div>
+			<span className='fixed bottom-0 right-0 m-8'>
+				<ThemeSwitcherButton height='h-8' />
+			</span>
 		</div>
 	);
 };
