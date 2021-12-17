64d7668075c414eb3621700d447879d6ecaf8cdf
Antonio Lo Fiego <alofiego@hey.com>
2021-12-16 21:15:28 -0500

    Rename main app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-
 import '@styles/tailwind.css';
 
 import type { AppProps } from 'next/app';
 
 import { ThemeProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider attribute='class' defaultTheme='system'>
 			<Component {...pageProps} />
 		</ThemeProvider>
 	);
 }
-export default MyApp;
+export default App;
