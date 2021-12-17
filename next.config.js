c96b4b2edd26727d36bf9b8c4f053ccd15794ddd
Antonio Lo Fiego <alofiego@hey.com>
2021-12-17 18:52:46 -0500

    Add Github and Discord as image providers for Next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	reactStrictMode: true,
 	images: {
-		domains: ['images.unsplash.com'],
+		domains: [
+			'images.unsplash.com',
+			'avatars.githubusercontent.com',
+			'cdn.discordapp.com',
+		],
 	},
 };
