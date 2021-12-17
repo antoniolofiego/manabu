7c7024f02b36c06c1b7edb638b878c15a3db86ca
Antonio Lo Fiego <alofiego@hey.com>
2021-12-16 22:26:10 -0500

    Create useSupabase hook

---
+++ hooks/useSupabase.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+	process.env.NEXT_PUBLIC_SUPABASE_API_KEY as string
+);
+
+const useSupabase = () => {
+	const [session, setSession] = useState(supabase.auth.session());
+
+	supabase.auth.onAuthStateChange((_event, session) => {
+		setSession(session);
+	});
+
+	return { session, supabase };
+};
+
+export default useSupabase;
