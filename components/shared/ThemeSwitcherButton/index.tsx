cb4c8ad4bcf92d4aafec993bcc5104c5407f6dd8
Antonio Lo Fiego <alofiego@hey.com>
2021-12-16 23:04:48 -0500

    Make ThemeSwitcherButton have custom height

--- components/shared/ThemeSwitcherButton/index.tsx
+++ components/shared/ThemeSwitcherButton/index.tsx
@@ -1,20 +1,26 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/solid';
 import { useTheme } from 'next-themes';
 
-const ThemeSwitcherButton: React.FC = () => {
+type ThemeSwitcherButtonProps = {
+	height?: string;
+};
+
+const ThemeSwitcherButton: React.FC<ThemeSwitcherButtonProps> = ({
+	height,
+}) => {
 	const { theme, setTheme, resolvedTheme } = useTheme();
 
 	return (
 		<button
 			data-testid='themeSwitcher'
-			className='flex items-center w-full text-sm rounded-md group'
+			className={`flex items-center text-sm rounded-md group ${height}`}
 			onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
 		>
 			{resolvedTheme === 'dark' && (
-				<SunIcon className={'h-6 w-6 text-gray-50'} />
+				<SunIcon className={'h-full w-full text-gray-50'} />
 			)}
 			{resolvedTheme === 'light' && (
-				<MoonIcon className={'h-6 w-6 text-gray-900'} />
+				<MoonIcon className={'h-full w-full text-gray-900'} />
 			)}
 		</button>
 	);
