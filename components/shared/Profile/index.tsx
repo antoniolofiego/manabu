29862fa64be6f2a4824ffe8168ba56b5909f357b
Antonio Lo Fiego <alofiego@hey.com>
2021-12-17 18:58:16 -0500

    Replace static profile picture with the identity provider one

--- components/shared/Profile/index.tsx
+++ components/shared/Profile/index.tsx
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Image from 'next/image';
 import Dropdown from './Dropdown';
+import { supabase } from '@utils/supabase';
 
 const Profile = () => {
+	const [imgUrl, setImgUrl] = useState('');
+
+	useEffect(() => {
+		const user = supabase.auth.user();
+		setImgUrl(user?.user_metadata.avatar_url);
+	}, []);
+
 	return (
 		<div className='flex flex-col'>
 			<Dropdown>
-				<button className='relative w-24 h-24 overflow-hidden rounded-full'>
-					<Image
-						alt='profile picture'
-						src='https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-						layout='fill'
-						objectFit='cover'
-					/>
-				</button>
+				<div className='relative w-24 h-24 overflow-hidden rounded-full'>
+					{imgUrl && (
+						<Image
+							alt='profile picture'
+							src={imgUrl}
+							layout='fill'
+							objectFit='cover'
+						/>
+					)}
+				</div>
 			</Dropdown>
 		</div>
 	);
