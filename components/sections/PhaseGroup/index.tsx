e2d882b9877545b2dc4f90458f4d35877f57367d
Antonio Lo Fiego <alofiego@hey.com>
2021-12-18 16:04:57 -0500

    Move Home page phases into its own component

---
+++ components/sections/PhaseGroup/index.tsx
@@ -0,0 +1,38 @@
+import Phase from '@components/Phase';
+
+const PhaseGroup = () => {
+	return (
+		<ul className='grid w-full gap-12 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:grid-rows-2'>
+			<Phase
+				number={1}
+				name='Linux & networking'
+				stage='3. Introduction to Networking'
+				progress={0.5}
+				url='intro-to-networking'
+			/>
+			<Phase
+				number={2}
+				name='Programming & Scripting'
+				stage='2. Automation with Python'
+				progress={0.33}
+				url='automation-with-python'
+			/>
+			<Phase
+				number={3}
+				name='Cloud Platforms'
+				stage='1. Cloud Service Providers'
+				progress={0.0}
+				url='cloud-service-providers'
+			/>
+			<Phase
+				number={4}
+				name='DevOps Fundamentals'
+				stage='1. DevOps concepts'
+				progress={0.0}
+				url='devops-concepts'
+			/>
+		</ul>
+	);
+};
+
+export default PhaseGroup;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,4 @@
-import Phase from '@components/Phase';
+import PhaseGroup from '@components/sections/PhaseGroup';
 import SearchBar from '@components/shared/SearchBar';
 import Layout from '@components/Layout';
 import Head from 'next/head';
@@ -37,45 +37,16 @@ const Home: React.FC = () => {
 			</Head>
 
 			<Layout>
-				<div className='grid items-center px-12 mx-auto space-y-16 max-w-7xl'>
+				<>
 					<SearchBar />
-					<h2 className='text-2xl font-extrabold justify-self-center lg:justify-self-start'>
-						Hello, {username}
-					</h2>
-					<ul className='grid w-full gap-12 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:grid-rows-2'>
-						<Phase
-							number={1}
-							name='Linux & networking'
-							stage='3. Introduction to Networking'
-							progress={0.5}
-							url='intro-to-networking'
-						/>
-						<Phase
-							number={2}
-							name='Programming & Scripting'
-							stage='2. Automation with Python'
-							progress={0.33}
-							url='automation-with-python'
-						/>
-						<Phase
-							number={3}
-							name='Cloud Platforms'
-							stage='1. Cloud Service Providers'
-							progress={0.0}
-							url='cloud-service-providers'
-						/>
-						<Phase
-							number={4}
-							name='DevOps Fundamentals'
-							stage='1. DevOps concepts'
-							progress={0.0}
-							url='devops-concepts'
-						/>
-					</ul>
-					<h2 className='text-2xl font-extrabold'>
-						Looking for something to do?
-					</h2>
-				</div>
+					<div className='grid items-center px-12 mx-auto space-y-16 max-w-7xl'>
+						<h2 className='text-2xl font-extrabold '>Hello, {username}</h2>
+						<PhaseGroup />
+						<h2 className='text-2xl font-extrabold'>
+							Looking for something to do?
+						</h2>
+					</div>
+				</>
 			</Layout>
 		</>
 	) : (
