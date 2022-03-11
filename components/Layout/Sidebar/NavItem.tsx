9ab99f230c529c451e290ca4f85c9c8dedeeb9df
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 13:18:11 -0500

    Spacing

--- components/Layout/Sidebar/NavItem.tsx
+++ components/Layout/Sidebar/NavItem.tsx
@@ -2,41 +2,41 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 type NavItemProps = {
-	name: string;
-	url: string;
+  name: string;
+  url: string;
 };
 
 const NavItem: React.FC<NavItemProps> = ({ name, url, children }) => {
-	const router = useRouter();
-	const pageIsActive = (page: string): boolean => {
-		return router.asPath.split('/')[1] === page.split('/')[1];
-	};
+  const router = useRouter();
+  const pageIsActive = (page: string): boolean => {
+    return router.asPath.split('/')[1] === page.split('/')[1];
+  };
 
-	const inactiveStyles =
-		'transition-all origin-left scale-0 group-hover:scale-100 duration-200';
+  const inactiveStyles =
+    'transition-all origin-left scale-0 group-hover:scale-100 duration-200';
 
-	return (
-		<Link href={url}>
-			<a className='relative w-full group active:translate-y-px'>
-				<span
-					className={`absolute left-0 w-2 h-full bg-gray-900 dark:bg-gray-50 rounded-r-lg ${
-						!pageIsActive(url) && inactiveStyles
-					}`}
-				/>
+  return (
+    <Link href={url}>
+      <a className='relative w-full group active:translate-y-px'>
+        <span
+          className={`absolute left-0 w-2 h-full bg-gray-900 dark:bg-gray-50 rounded-r-lg ${
+            !pageIsActive(url) && inactiveStyles
+          }`}
+        />
 
-				<span
-					className={`flex items-center pl-12 space-x-4 ${
-						pageIsActive(url)
-							? 'dark:text-gray-200'
-							: 'active: translate-px dark:group-hover:text-gray-200 group-hover:text-gray-800'
-					}`}
-				>
-					{children}
-					<span>{name || 'Home'}</span>
-				</span>
-			</a>
-		</Link>
-	);
+        <span
+          className={`flex items-center pl-12 space-x-4 ${
+            pageIsActive(url)
+              ? 'dark:text-gray-200'
+              : 'active: translate-px dark:group-hover:text-gray-200 group-hover:text-gray-800'
+          }`}
+        >
+          {children}
+          <span>{name || 'Home'}</span>
+        </span>
+      </a>
+    </Link>
+  );
 };
 
 export default NavItem;
