411de619bac18432b78f54fb15b1fe658c432989
Antonio Lo Fiego <alofiego@hey.com>
2022-03-10 20:03:05 -0500

    Spacing

--- pages/home.tsx
+++ pages/home.tsx
@@ -7,51 +7,51 @@ import { supabase } from '@utils/supabase';
 import useUnauthenticatedPush from '@hooks/useUnauthenticatedPush';
 
 const Home: React.FC = () => {
-	const loggedIn = useUnauthenticatedPush(true);
+  const loggedIn = useUnauthenticatedPush(true);
 
-	const user = supabase.auth.user();
-	console.log(user);
+  const user = supabase.auth.user();
+  console.log(user);
 
-	let username: string;
+  let username: string;
 
-	switch (user?.app_metadata.provider) {
-		case 'discord':
-			username = user?.user_metadata.name;
-			break;
-		case 'github':
-			username = user?.user_metadata.user_name;
-		default:
-			username = 'friend';
-			break;
-	}
+  switch (user?.app_metadata.provider) {
+    case 'discord':
+      username = user?.user_metadata.name;
+      break;
+    case 'github':
+      username = user?.user_metadata.user_name;
+    default:
+      username = 'friend';
+      break;
+  }
 
-	return loggedIn ? (
-		<>
-			<Head>
-				<title>Home | LearnToCloud</title>
-				<meta
-					name='description'
-					content='Generated with the Batteries Included Next App template'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+  return loggedIn ? (
+    <>
+      <Head>
+        <title>Home | LearnToCloud</title>
+        <meta
+          name='description'
+          content='Generated with the Batteries Included Next App template'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-			<Layout>
-				<>
-					<SearchBar />
-					<div className='grid items-center px-12 mx-auto space-y-16 max-w-7xl'>
-						<h2 className='text-2xl font-extrabold '>Hello, {username}</h2>
-						<PhaseGroup />
-						<h2 className='text-2xl font-extrabold'>
-							Looking for something to do?
-						</h2>
-					</div>
-				</>
-			</Layout>
-		</>
-	) : (
-		<p>Unathorized!</p>
-	);
+      <Layout>
+        <>
+          <SearchBar />
+          <div className='grid items-center px-12 mx-auto space-y-16 max-w-7xl'>
+            <h2 className='text-2xl font-extrabold '>Hello, {username}</h2>
+            <PhaseGroup />
+            <h2 className='text-2xl font-extrabold'>
+              Looking for something to do?
+            </h2>
+          </div>
+        </>
+      </Layout>
+    </>
+  ) : (
+    <p>Unathorized!</p>
+  );
 };
 
 export default Home;
