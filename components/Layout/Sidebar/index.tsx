7b9782699cf6c1622f2012260797a089f9b7eccb
Antonio Lo Fiego <alofiego@hey.com>
2022-03-10 21:02:22 -0500

    Use UserContext to change navbar behavior

--- components/Layout/Sidebar/index.tsx
+++ components/Layout/Sidebar/index.tsx
@@ -3,39 +3,50 @@ import Profile from '@components/shared/Profile';
 import NavItem from './NavItem';
 
 import {
-	HomeIcon,
-	MapIcon,
-	LightBulbIcon,
-	CollectionIcon,
-	CogIcon,
+  HomeIcon,
+  MapIcon,
+  LightBulbIcon,
+  CollectionIcon,
+  CogIcon,
+  LoginIcon,
 } from '@heroicons/react/outline';
 
+import { useUser } from '@context/user';
+
 const Sidebar = () => {
-	return (
-		<nav className='flex flex-col items-center justify-between w-56 h-screen py-[5vh] border-r transition-all border-gray-900 dark:border-gray-50 sticky top-0 z-20'>
-			<Logo />
+  const { user } = useUser();
 
-			<ul className='flex flex-col items-center w-full space-y-16'>
-				<NavItem name='Home' url='/home'>
-					<HomeIcon className='h-8' />
-				</NavItem>
-				<NavItem name='Guide' url='/guide'>
-					<MapIcon className='h-8' />
-				</NavItem>
-				<NavItem name='Projects' url='/projects'>
-					<LightBulbIcon className='h-8' />
-				</NavItem>
-				<NavItem name='Resources' url='/resources'>
-					<CollectionIcon className='h-8' />
-				</NavItem>
-				<NavItem name='Settings' url='/settings'>
-					<CogIcon className='h-8' />
-				</NavItem>
-			</ul>
+  return (
+    <nav className='flex flex-col items-center justify-between w-56 h-screen py-[5vh] border-r transition-all border-gray-900 dark:border-gray-50 sticky top-0 z-20'>
+      <Logo className='text-3xl' />
 
-			<Profile />
-		</nav>
-	);
+      <ul className='flex flex-col items-center w-full space-y-16'>
+        <NavItem name='Home' url='/home'>
+          <HomeIcon className='h-8' />
+        </NavItem>
+        <NavItem name='Guide' url='/guide'>
+          <MapIcon className='h-8' />
+        </NavItem>
+        <NavItem name='Projects' url='/projects'>
+          <LightBulbIcon className='h-8' />
+        </NavItem>
+        <NavItem name='Resources' url='/resources'>
+          <CollectionIcon className='h-8' />
+        </NavItem>
+        {user ? (
+          <NavItem name='Settings' url='/settings'>
+            <CogIcon className='h-8' />
+          </NavItem>
+        ) : (
+          <NavItem name='Login' url='/login'>
+            <LoginIcon className='h-8' />
+          </NavItem>
+        )}
+      </ul>
+
+      <Profile />
+    </nav>
+  );
 };
 
 export default Sidebar;
