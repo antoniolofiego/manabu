a09c44a33d5e1d636e9c6c6baad5209ea2b0b418
Antonio Lo Fiego <alofiego@hey.com>
2022-03-10 20:08:25 -0500

    Wrap _app with UserContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@styles/tailwind.css';
 
 import type { AppProps } from 'next/app';
+import UserProvider from '@context/user';
 
 import { ThemeProvider } from 'next-themes';
 
 function App({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider attribute='class' defaultTheme='system'>
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+  return (
+    <UserProvider>
+      <ThemeProvider attribute='class' defaultTheme='system'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UserProvider>
+  );
 }
 export default App;
