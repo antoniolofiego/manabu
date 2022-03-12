0105295316856d5b6d40318dc5605078c8a63614
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 22:47:23 -0500

    Move header inside CardGroup

--- components/sections/CardGroup/index.tsx
+++ components/sections/CardGroup/index.tsx
@@ -6,10 +6,14 @@ import {
 
 export const CardGroup = () => {
   return (
-    <div className='grid grid-cols-3 gap-8'>
-      <FindACourseCard />
-      <ArticleCard />
-      <ShareProgressCard />
+    <div className='space-y-8'>
+      <h2 className='text-2xl font-extrabold'>Looking for something to do?</h2>
+
+      <div className='grid grid-cols-3 gap-8'>
+        <FindACourseCard />
+        <ArticleCard />
+        <ShareProgressCard />
+      </div>
     </div>
   );
 };
