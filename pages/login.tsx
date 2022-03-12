6d57b1149ebbd13578e4ca680a5bc67736b8bd16
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 22:37:45 -0500

    Add Home button to login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,10 @@
-import { Logo } from '@components/assets/';
+import { Logo } from '@components/assets';
 import { ThemeSwitcherButton } from '@components/elements';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import { BsGithub, BsDiscord } from 'react-icons/bs';
+import { HomeIcon } from '@heroicons/react/outline';
 
 import { useUser } from '@context/user';
 import { useRouter } from 'next/router';
@@ -26,6 +28,14 @@ const Login: NextPage = () => {
         <title>Login | manabu</title>
       </Head>
       <div className='relative flex flex-col items-center justify-center h-screen min-w-0 space-y-8 bg-gray-50 dark:bg-gray-900'>
+        <div className='absolute top-0 left-0 p-12'>
+          <Link href='/home'>
+            <a className='flex items-center space-x-4'>
+              <HomeIcon className='h-8 transition dark:hover:text-gray-400 hover:text-gray-800' />
+            </a>
+          </Link>
+        </div>
+
         <Logo className='text-4xl text-gray-900 dark:text-gray-50' />
         <div className='flex flex-col space-y-6'>
           <button
