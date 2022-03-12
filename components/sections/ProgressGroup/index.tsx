c600b5bf1b5ccff6cc8181870555d309d82dd533
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 22:51:17 -0500

    Add horizontal padding

--- components/sections/ProgressGroup/index.tsx
+++ components/sections/ProgressGroup/index.tsx
@@ -5,7 +5,7 @@ export const ProgressGroup = () => {
     <div className='space-y-8'>
       <h2 className='text-2xl font-extrabold'>Keep making progress</h2>
 
-      <ul className='grid w-full gap-12 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:grid-rows-2'>
+      <ul className='grid w-full gap-12 px-8 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:grid-rows-2'>
         <ProgressCard
           name='Linux & networking'
           courseID='sand8cs9a8'
