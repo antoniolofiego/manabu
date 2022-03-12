32d3c22c26835d6eb52a70f322485d7026808735
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 21:15:44 -0500

    Add ThemeSwitcherButton to Layout

--- components/sections/Layout/index.tsx
+++ components/sections/Layout/index.tsx
@@ -1,5 +1,4 @@
-import Sidebar from '../../elements/Sidebar';
-import { ThemeSwitcherButton } from '@components/elements';
+import { ThemeSwitcherButton, SearchBar, Sidebar } from '@components/elements';
 
 type LayoutProps = { children: JSX.Element };
 
@@ -7,10 +6,15 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='relative flex min-h-screen transition-all bg-gray-50 dark:bg-gray-900 dark:text-gray-50'>
       <Sidebar />
-      <div className='flex-1 min-w-0 overflow-auto'>{children}</div>
-      <span className='fixed bottom-0 right-0 m-8'>
-        <ThemeSwitcherButton height='h-8' />
-      </span>
+      <div className='flex-1 max-w-5xl min-w-0 mx-auto overflow-auto'>
+        <div className='flex items-center justify-between w-3/4 mx-auto space-x-12'>
+          <SearchBar />
+          <span>
+            <ThemeSwitcherButton height='h-8' />
+          </span>
+        </div>
+        {children}
+      </div>
     </div>
   );
 };
