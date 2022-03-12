9eed551afc51dcd8b38fa35cd6a5bc41cc1f774e
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 19:36:10 -0500

    Homepage pushes to /home instead of /login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,12 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useUser } from '@context/user';
 
 const Home: React.FC = () => {
-  const { user } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (!!user) {
-      router.push('/home');
-    }
-
-    if (!user) {
-      router.push('/login');
-    }
-  }, [router, user]);
+    router.push('/home');
+  }, [router]);
 
   return <></>;
 };
