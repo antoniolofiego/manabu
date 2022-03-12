a3cba4c91a083c32f93fc1efc65d9a4b56cefde0
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 20:35:24 -0500

    Stub Tag component

--- components/elements/index.tsx
+++ components/elements/index.tsx
@@ -6,4 +6,5 @@ export {
 } from './cards';
 export { Profile } from './Profile';
 export { SearchBar } from './SearchBar';
+export { Tag } from './Tag';
 export { ThemeSwitcherButton } from './ThemeSwitcherButton';
