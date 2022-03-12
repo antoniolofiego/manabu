ed9dfa07d41500d6a176bf065528e92896192234
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 23:12:34 -0500

    Replace hard-coded social logins with SocialLogin component

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@components/assets';
 import { ThemeSwitcherButton } from '@components/elements';
+import { SocialLogin } from '@components/sections';
 import Head from 'next/head';
 import Link from 'next/link';
 
@@ -37,26 +38,7 @@ const Login: NextPage = () => {
         </div>
 
         <Logo className='text-4xl text-gray-900 dark:text-gray-50' />
-        <div className='flex flex-col space-y-6'>
-          <button
-            onClick={loginGithub}
-            className='flex items-center px-5 py-3 space-x-8 bg-gray-900 shadow-lg rounded-xl shadow-gray-900/30 dark:bg-gray-50 dark:shadow-gray-50/30 hover:bg-gray-700 dark:hover:bg-gray-200'
-          >
-            <BsGithub size={32} className='text-gray-50 dark:text-gray-900' />
-            <span className='text-gray-50 dark:text-gray-900'>
-              Sign in with GitHub
-            </span>
-          </button>
-          <button
-            onClick={loginDiscord}
-            className='flex items-center px-5 py-3 space-x-8 bg-gray-900 shadow-lg rounded-xl shadow-gray-900/30 dark:bg-gray-50 dark:shadow-gray-50/30 hover:bg-gray-700 dark:hover:bg-gray-200'
-          >
-            <BsDiscord size={32} className='text-gray-50 dark:text-gray-900' />
-            <span className='text-gray-50 dark:text-gray-900'>
-              Sign in with Discord
-            </span>
-          </button>
-        </div>
+        <SocialLogin />
         <div className='absolute bottom-0 right-0 p-12'>
           <ThemeSwitcherButton height={'h-8'} />
         </div>
