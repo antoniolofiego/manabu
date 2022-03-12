abafe92ae11f14ab0352c93a3bcb0e4fe97e2251
Antonio Lo Fiego <alofiego@hey.com>
2022-03-12 13:39:21 -0800

    Add global horizontal padding to main content

--- components/sections/Layout/index.tsx
+++ components/sections/Layout/index.tsx
@@ -6,7 +6,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='relative flex min-h-screen transition-all bg-gray-50 dark:bg-gray-900 dark:text-gray-50'>
       <Sidebar />
-      <div className='flex-1 max-w-5xl min-w-0 mx-auto overflow-auto'>
+      <main className='flex-1 max-w-6xl min-w-0 px-12 mx-auto overflow-auto'>
         <div className='flex items-center justify-between w-3/4 mx-auto space-x-12'>
           <SearchBar />
           <span>
@@ -14,7 +14,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
           </span>
         </div>
         {children}
-      </div>
+      </main>
     </div>
   );
 };
