81c66713c3c7eadb50e3ac9b20edfa1f6a308ce1
Antonio Lo Fiego <alofiego@hey.com>
2022-03-11 21:14:05 -0500

    Make searchbar take as much space as possible

--- components/elements/SearchBar/index.tsx
+++ components/elements/SearchBar/index.tsx
@@ -5,10 +5,10 @@ export const SearchBar: React.FC = () => {
   const [searchTerms, setSearchTerms] = useState('');
 
   return (
-    <div className='flex items-center w-full max-w-2xl mx-auto my-8 border border-black rounded-2xl group focus-within:ring-2 dark:border-gray-50'>
+    <div className='flex items-center flex-1 my-8 border border-black rounded-2xl group focus-within:ring-2 dark:border-gray-50'>
       <SearchIcon className='h-4 pl-4' />
       <input
-        className='w-full px-4 py-2 mx-auto transition-all focus:outline-none dark:bg-gray-900 rounded-2xl dark:placeholder-gray-200'
+        className='w-full px-4 py-2 mx-auto transition-all focus:outline-none dark:bg-gray-900 rounded-2xl dark:placeholder-gray-400'
         placeholder='Search'
         value={searchTerms}
         onChange={(e: React.FormEvent<HTMLInputElement>) =>
