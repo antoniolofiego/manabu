419cd1e6614a82cd3befb85e26ee060da8e838d1
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 16:34:55 -0700

    Fetch lesson detail data from backend

--- pages/courses/[id]/index.tsx
+++ pages/courses/[id]/index.tsx
@@ -4,18 +4,23 @@ import { supabase } from '@utils/supabase';
 
 import type { ParsedUrlQuery } from 'querystring';
 import type { NextPage, GetStaticProps } from 'next';
-import type { CourseDetails, InstructorDetails } from '@types';
+import type { CourseDetails, InstructorDetails, LessonDetails } from '@types';
 
 interface ICoursePageProps {
   course: CourseDetails;
   instructor: InstructorDetails;
+  lessons: LessonDetails[];
 }
 
 interface IParams extends ParsedUrlQuery {
   slug: string;
 }
 
-const CoursePage: NextPage<ICoursePageProps> = ({ course, instructor }) => {
+const CoursePage: NextPage<ICoursePageProps> = ({
+  course,
+  instructor,
+  lessons,
+}) => {
   console.log(course);
   return (
     <Layout>
@@ -23,7 +28,11 @@ const CoursePage: NextPage<ICoursePageProps> = ({ course, instructor }) => {
         <span className='px-4 py-2 bg-green-400 rounded-full text-slate-700'>
           Course
         </span>
-        <CourseSplash course={course} instructor={instructor} />
+        <CourseSplash
+          course={course}
+          instructor={instructor}
+          lessons={lessons}
+        />
       </div>
     </Layout>
   );
@@ -46,33 +55,56 @@ export async function getStaticPaths() {
 export const getStaticProps: GetStaticProps = async (context) => {
   const { id } = context.params as IParams;
 
-  const { data, error } = await supabase.rpc('get_course_details', {
-    courseid: id,
-  });
+  const { data: courseData, error: courseError } = await supabase.rpc(
+    'get_course_details',
+    {
+      courseid: id,
+    }
+  );
 
-  console.log(data);
-
-  if (error) {
-    console.error(error);
+  if (courseError) {
+    console.error(courseError);
     return { notFound: true };
   }
 
-  if (data) {
+  const { data: lessonsData, error: lessonError } = await supabase.rpc(
+    'get_lessons_for_course',
+    {
+      courseid: id,
+    }
+  );
+
+  if (lessonError) {
+    console.error(lessonError);
+    return { notFound: true };
+  }
+
+  if (courseData && lessonsData) {
     const course = {
-      name: data[0].course_name,
-      id: data[0].course_id,
-      imageUrl: data[0].course_image_url,
-      description: data[0].course_description,
+      name: courseData[0].course_name,
+      id: courseData[0].course_id,
+      imageUrl: courseData[0].course_image_url,
+      description: courseData[0].course_description,
     };
 
     const instructor = {
-      name: data[0].instructor_name,
-      id: data[0].instructor_id,
-      imageUrl: data[0].instructor_image_url,
+      name: courseData[0].instructor_name,
+      id: courseData[0].instructor_id,
+      imageUrl: courseData[0].instructor_image_url,
     };
 
+    const lessons = lessonsData.map((lesson) => {
+      return {
+        name: lesson.name,
+        id: lesson.id,
+        imageUrl: lesson.image_url,
+        lessonNumber: lesson.lesson_number,
+        videoUrl: lesson.video_url,
+      };
+    });
+
     return {
-      props: { course, instructor },
+      props: { course, instructor, lessons },
     };
   }
 
