e713733df8e5b51174654a41c8eea070e912550b
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 15:58:14 -0700

    Separate CourseDetails into two types

--- types/CourseDetails.ts
+++ types/CourseDetails.ts
@@ -1,9 +1,12 @@
 export type CourseDetails = {
-  course_name: string;
-  course_id: string;
-  course_image_url: string;
-  course_description: string;
-  instructor_name: string;
-  instructor_id: string;
-  instructor_image_url: string;
+  name: string;
+  id: string;
+  image_url?: string;
+  description: string;
+};
+
+export type InstructorDetails = {
+  name: string;
+  id: string;
+  image_url?: string;
 };
--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,2 @@
-export type { CourseDetails } from './CourseDetails';
+export type { CourseDetails, InstructorDetails } from './CourseDetails';
 export type { LessonProgress } from './LessonProgress';
