65ddf25df807ec6406ecfa171a176083ef505b5a
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 01:21:15 -0700

    Use Lessons from backend instead of hardcoded ones

--- components/sections/ProgressGroup/index.tsx
+++ components/sections/ProgressGroup/index.tsx
@@ -1,51 +1,66 @@
+import { useState, useEffect } from 'react';
+
 import { ProgressCard } from '@components/elements';
 
+import { supabase } from '@utils/supabase';
+import { useUser } from '@context/user';
+import type { LessonProgress } from '@types';
+
 export const ProgressGroup = () => {
+  const [lessons, setLessons] = useState<LessonProgress[]>([]);
+  const { user } = useUser();
+  console.log(user?.id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let { data, error } = await supabase.rpc('next_lessons_for_user', {
+        user_id: user?.id,
+      });
+
+      if (data) {
+        const formattedData: LessonProgress[] = data?.map((lesson) => {
+          return {
+            id: lesson.id,
+            name: lesson.name,
+            lessonNumber: lesson.lesson_number,
+            imageUrl: lesson.image_url,
+            progress: lesson.progress,
+            instructorName: lesson.instructor_name,
+            instructorId: lesson.instructor_id,
+            courseName: lesson.course_name,
+            courseId: lesson.course_id,
+          };
+        });
+        setLessons(() => formattedData);
+      }
+
+      if (error) console.error(error);
+    };
+
+    fetchData();
+  }, [user]);
+
+  // console.log(data);
   return (
     <div className='space-y-8'>
       <h2 className='text-2xl font-extrabold'>Keep making progress</h2>
 
-      <ul className='grid w-full gap-12 px-8 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2 lg:grid-rows-2'>
-        <ProgressCard
-          name='Linux & networking'
-          courseID='sand8cs9a8'
-          instructor='Bill Nye'
-          instructorID='iasdhiah'
-          stage='3. Introduction to Networking'
-          progress={0.5}
-          url='intro-to-networking'
-          imageUrl='https://images.unsplash.com/photo-1518432031352-d6fc5c10da5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGludXh8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
-        />
-        <ProgressCard
-          name='Programming & Scripting'
-          courseID='caiushcsa8231'
-          instructor='Michael Myers'
-          instructorID='89asc8as90c8'
-          stage='2. Automation with Python'
-          progress={0.33}
-          url='automation-with-python'
-          imageUrl='https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80'
-        />
-        <ProgressCard
-          name='Cloud Platforms'
-          courseID='uhsaidoua823'
-          instructor='Corey Quinn'
-          instructorID='doaisjd0as98'
-          stage='1. Cloud Service Providers'
-          progress={0.0}
-          url='cloud-service-providers'
-          imageUrl='https://images.unsplash.com/photo-1509803874385-db7c23652552?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdWQlMjBjb21wdXRpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
-        />
-        <ProgressCard
-          name='DevOps Fundamentals'
-          courseID='nidas9213'
-          instructor='Paris Hilton'
-          instructorID='adsoih0asjop'
-          stage='1. DevOps concepts'
-          progress={0.0}
-          url='devops-concepts'
-          imageUrl='https://images.unsplash.com/photo-1571786256017-aee7a0c009b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VydmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-        />
+      <ul className='grid w-full gap-12 px-8 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2'>
+        {lessons.map((lesson) => {
+          return (
+            <ProgressCard
+              key={lesson.id}
+              name={lesson.courseName}
+              courseID={lesson.courseId}
+              instructor={lesson.instructorName}
+              instructorID={lesson.instructorId}
+              stage={`${lesson.lessonNumber}. ${lesson.name}`}
+              progress={lesson.progress}
+              url={lesson.id}
+              imageUrl={lesson.imageUrl}
+            />
+          );
+        })}
       </ul>
     </div>
   );
