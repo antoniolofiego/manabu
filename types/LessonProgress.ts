f60915f3b6743208d073a0d2522511f8cccc5901
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 01:21:32 -0700

    Create LessonProgress type

---
+++ types/LessonProgress.ts
@@ -0,0 +1,11 @@
+export type LessonProgress = {
+  id: string;
+  name: string;
+  lessonNumber: number;
+  imageUrl: string;
+  progress: number;
+  courseId: string;
+  courseName: string;
+  instructorName: string;
+  instructorId: string;
+};
---
+++ types/index.ts
@@ -0,0 +1,1 @@
+export type { LessonProgress } from './LessonProgress';
