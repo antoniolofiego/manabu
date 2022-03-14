f79918438fecc2e706a5784650c89966e62a9256
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 12:37:27 -0700

    Implement ErrorBoundary in ProgressGroup

--- components/sections/ProgressGroup/index.tsx
+++ components/sections/ProgressGroup/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import { ProgressCard } from '@components/elements';
+import { ErrorBoundary, useErrorHandler } from 'react-error-boundary';
 
 import { supabase } from '@utils/supabase';
 import { useUser } from '@context/user';
@@ -10,7 +11,8 @@ export const ProgressGroup = () => {
   const [lessons, setLessons] = useState<LessonProgress[]>([]);
 
   const { user } = useUser();
-  console.log(user?.id);
+
+  const handleError = useErrorHandler();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,34 +37,50 @@ export const ProgressGroup = () => {
         setLessons(() => formattedData);
       }
 
-      if (error) console.error(error);
+      if (error) handleError(error);
     };
 
     fetchData();
-  }, [user]);
+  }, [user, handleError]);
 
-  // console.log(data);
   return (
-    <div className='space-y-8'>
-      <h2 className='text-2xl font-extrabold'>Keep making progress</h2>
+    user &&
+    lessons.length > 0 && (
+      <div className='space-y-8'>
+        <h2 className='text-2xl font-extrabold'>Keep making progress</h2>
 
-      <ul className='grid w-full gap-12 px-8 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2'>
-        {lessons.map((lesson) => {
-          return (
-            <ProgressCard
-              key={lesson.id}
-              name={lesson.courseName}
-              courseID={lesson.courseId}
-              instructor={lesson.instructorName}
-              instructorID={lesson.instructorId}
-              stage={`${lesson.lessonNumber}. ${lesson.name}`}
-              progress={lesson.progress}
-              url={lesson.id}
-              imageUrl={lesson.imageUrl}
-            />
-          );
-        })}
-      </ul>
-    </div>
+        <ul className='grid w-full gap-12 px-8 mx-auto lg:mx-0 lg:max-w-full lg:grid-cols-2'>
+          <ErrorBoundary
+            fallback={
+              <p>
+                We had troubles loading your courses!{' '}
+                <span
+                  className='font-bold transition cursor-pointer hover:text-gray-700 dark:hover:text-gray-400'
+                  onClick={() => location.reload()}
+                >
+                  Please try refreshing.
+                </span>
+              </p>
+            }
+          >
+            {lessons.map((lesson) => {
+              return (
+                <ProgressCard
+                  key={lesson.id}
+                  name={lesson.courseName}
+                  courseID={lesson.courseId}
+                  instructor={lesson.instructorName}
+                  instructorID={lesson.instructorId}
+                  stage={`${lesson.lessonNumber}. ${lesson.name}`}
+                  progress={lesson.progress}
+                  url={lesson.id}
+                  imageUrl={lesson.imageUrl}
+                />
+              );
+            })}
+          </ErrorBoundary>
+        </ul>
+      </div>
+    )
   );
 };
