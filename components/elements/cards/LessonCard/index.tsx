a8ef353761435ddf26163ef2b535f0a121e3f90b
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 16:50:22 -0700

    Create LessonCard

--- components/elements/cards/LessonCard/index.tsx
+++ components/elements/cards/LessonCard/index.tsx
@@ -1,9 +1,25 @@
+import Link from 'next/link';
 import type { LessonDetails } from '@types';
 
 interface ILessonCardProps {
   lesson: LessonDetails;
+  courseId: string;
 }
 
-export const LessonCard: React.FC<ILessonCardProps> = ({ lesson }) => {
-  return <div>{lesson.name}</div>;
+export const LessonCard: React.FC<ILessonCardProps> = ({
+  lesson,
+  courseId,
+}) => {
+  return (
+    <div className='flex items-center space-x-4 transition group hover:translate-x-2'>
+      <p className='dark:group-hover:text-gray-400 group-hover:text-gray-700'>
+        #{lesson.lessonNumber}
+      </p>
+      <Link href={`/courses/${courseId}/${lesson.id}`}>
+        <a className='text-2xl dark:group-hover:text-gray-400 group-hover:text-gray-700'>
+          {lesson.name}
+        </a>
+      </Link>
+    </div>
+  );
 };
