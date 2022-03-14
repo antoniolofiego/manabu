df2bda0ed6a9ab92089cbea3037542b375706632
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 16:50:33 -0700

    Create Instructor badge

--- components/elements/InstructorBadge/index.tsx
+++ components/elements/InstructorBadge/index.tsx
@@ -1,3 +1,33 @@
-export const InstructorBadge = () => {
-  return <div></div>;
+import Image from 'next/image';
+import Link from 'next/link';
+
+import type { InstructorDetails } from '@types';
+interface IInstructorBadgeProps {
+  instructor: InstructorDetails;
+}
+
+export const InstructorBadge: React.FC<IInstructorBadgeProps> = ({
+  instructor,
+}) => {
+  return (
+    <div className='flex items-center space-x-4'>
+      <div className='relative w-24 h-24 overflow-hidden rounded-full'>
+        {instructor.imageUrl ? (
+          <Image
+            src={instructor.imageUrl}
+            layout='fill'
+            alt={`${instructor.name} profile picture`}
+          />
+        ) : (
+          <span className='absolute w-full h-full bg-gradient-to-br from-red-500 to-blue-500' />
+        )}
+      </div>
+      <div>
+        <p className='font-mono text-sm capitalize'>Instructor</p>
+        <Link href={`/instructors/${instructor.id}`}>
+          <a className='text-lg'>{instructor.name}</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
