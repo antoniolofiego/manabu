b2b7d687bdaa5e8f5945fa6d6d080fd5c4b38323
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 14:17:26 -0700

    Remove unsed imports

--- components/elements/index.tsx
+++ components/elements/index.tsx
@@ -1,12 +1,6 @@
-export {
-  ArticleCard,
-  FindACourseCard,
-  ProgressCard,
-  ShareProgressCard,
-} from './cards';
+export { FindACourseCard, ProgressCard, ShareProgressCard } from './cards';
 export { Profile } from './Profile';
 export { SearchBar } from './SearchBar';
 export { Sidebar } from './Sidebar';
 export { SocialLoginButton } from './SocialLoginButton';
-export { Tag } from './Tag';
 export { ThemeSwitcherButton } from './ThemeSwitcherButton';
