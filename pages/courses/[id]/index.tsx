c02996e247e3853bb068f7fc84dbdf3186b8a5d5
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 01:22:23 -0700

    Create stub of pages

---
+++ pages/courses/[id]/index.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@components/sections';
+
+import type { NextPage } from 'next';
+
+const CoursePage: NextPage = () => {
+  return (
+    <Layout>
+      <div></div>
+    </Layout>
+  );
+};
+
+export default CoursePage;
