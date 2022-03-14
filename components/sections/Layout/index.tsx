b351ba27abfefafd001c516c231597089da64799
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 16:52:59 -0700

    Add small bottom padding in the Layout

--- components/sections/Layout/index.tsx
+++ components/sections/Layout/index.tsx
@@ -7,7 +7,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='relative flex min-h-screen transition-all bg-gray-50 dark:bg-gray-900 dark:text-gray-50'>
       <Sidebar />
-      <main className='flex-1 max-w-6xl min-w-0 px-12 mx-auto overflow-auto'>
+      <main className='flex-1 max-w-6xl min-w-0 px-12 mx-auto mb-8 overflow-auto'>
         <div className='flex items-center justify-between w-3/4 mx-auto space-x-12'>
           <SearchBar />
           <span>
