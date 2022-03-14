ec54029abdadd6905639f2a051e08420af24bcda
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 00:04:40 -0700

    Add image optimization domains from backend

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: [
-			'images.unsplash.com',
-			'avatars.githubusercontent.com',
-			'cdn.discordapp.com',
-		],
-	},
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'avatars.githubusercontent.com',
+      'cdn.discordapp.com',
+      'i3.ytimg.com',
+      'gvdativukredzrupkqgs.supabase.in',
+    ],
+  },
 };
