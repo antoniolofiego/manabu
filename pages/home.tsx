e63429b7c449891d8085224650d0d86a801ee836
Antonio Lo Fiego <alofiego@hey.com>
2022-03-15 17:06:24 -0400

    Add most recent courses to Homepage

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,17 @@
-import { ProgressGroup, CardGroup, Layout } from '@components/sections';
+import {
+  ProgressGroup,
+  CardGroup,
+  Layout,
+  CourseGroup,
+} from '@components/sections';
 import Head from 'next/head';
 
-import type { NextPage } from 'next';
+import { supabase } from '@utils/supabase';
 
-import React from 'react';
+import type { NextPage, GetStaticProps } from 'next';
+import type { ICourse, ICourseGroup } from '@types';
 
-const Home: NextPage = () => {
+const Home: NextPage<ICourseGroup> = ({ courses }) => {
   return (
     <>
       <Head>
@@ -20,6 +26,7 @@ const Home: NextPage = () => {
       <Layout>
         <div className='mx-auto space-y-12'>
           <ProgressGroup />
+          <CourseGroup title='Most recent courses' courses={courses} />
           <CardGroup />
         </div>
       </Layout>
@@ -28,3 +35,40 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data, error } = await supabase
+    .from('courses')
+    .select('*, instructors (*)', {
+      count: 'exact',
+    })
+    .order('created_at', { ascending: false })
+    .limit(2);
+
+  if (error) {
+    console.error(error);
+    return { notFound: true };
+  }
+
+  if (data) {
+    const courses: ICourse[] = data?.map((course) => {
+      return {
+        id: course.id,
+        name: course.name,
+        shortDescription: course.short_description,
+        imageUrl: course.image_url,
+        instructor: {
+          id: course.instructors.id,
+          name: course.instructors.name,
+          imageUrl: course.instructors.image_url,
+        },
+      };
+    });
+
+    return {
+      props: { courses },
+    };
+  }
+
+  return { notFound: true };
+};
