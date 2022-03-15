a27ce60a085feb77669f0f036a2e119f1fc9aa1f
Antonio Lo Fiego <alofiego@hey.com>
2022-03-15 17:29:03 -0400

    Make CardGroup responsive

--- components/sections/CardGroup/index.tsx
+++ components/sections/CardGroup/index.tsx
@@ -9,7 +9,7 @@ export const CardGroup = () => {
     <div className='space-y-8'>
       <Heading text='Looking for something to do?' />
 
-      <div className='grid grid-cols-2 gap-8 px-8'>
+      <div className='grid grid-cols-1 gap-8 px-8 lg:grid-cols-2'>
         <FindACourseCard />
         <ShareProgressCard />
       </div>
