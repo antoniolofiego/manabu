b9ee1b152b73b7906209f7444dadeceef877bf7b
Antonio Lo Fiego <alofiego@hey.com>
2022-03-15 16:39:42 -0400

    Create courses page

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,13 +1,65 @@
+import { CourseCard } from '@components/elements';
 import { Layout } from '@components/sections';
 
-import type { NextPage } from 'next';
+import { supabase } from '@utils/supabase';
 
-const Courses: NextPage = () => {
+import type { NextPage, GetStaticProps } from 'next';
+import type { CourseDetails, InstructorDetails } from '@types';
+
+interface Course extends CourseDetails {
+  instructor: InstructorDetails;
+}
+interface ICoursesPageProps {
+  courses: Course[];
+}
+
+const CoursesPage: NextPage<ICoursesPageProps> = ({ courses }) => {
+  console.log(courses[0]);
   return (
     <Layout>
-      <div></div>
+      <div className='grid gap-8 lg:grid-cols-2'>
+        {courses.map((course) => {
+          return <CourseCard course={course} key={course.id} />;
+        })}
+      </div>
     </Layout>
   );
 };
 
-export default Courses;
+export default CoursesPage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data, error } = await supabase
+    .from('courses')
+    .select('*, instructors (*), lessons( total_lessons ) ', {
+      count: 'exact',
+    })
+    .range(0, 9);
+
+  if (error) {
+    console.error(error);
+    return { notFound: true };
+  }
+
+  if (data) {
+    const courses: Course[] = data?.map((course) => {
+      return {
+        id: course.id,
+        name: course.name,
+        shortDescription: course.short_description,
+        imageUrl: course.image_url,
+        instructor: {
+          id: course.instructors.id,
+          name: course.instructors.name,
+          imageUrl: course.instructors.image_url,
+        },
+      };
+    });
+
+    return {
+      props: { courses },
+    };
+  }
+
+  return { notFound: true };
+};
