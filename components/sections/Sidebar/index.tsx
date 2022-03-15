31f29eab77cb85dabca2e10a4fc916b050d57bac
Antonio Lo Fiego <alofiego@hey.com>
2022-03-14 18:30:00 -0700

    Make sidebar responsive at smalller breakpoints

--- components/sections/Sidebar/index.tsx
+++ components/sections/Sidebar/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Logo } from '@components/assets/';
 import { UserProfile, NavItem } from '@components/elements';
 
@@ -5,48 +6,90 @@ import {
   HomeIcon,
   MapIcon,
   LightBulbIcon,
-  CollectionIcon,
   CogIcon,
   LoginIcon,
+  MenuIcon,
+  XIcon,
 } from '@heroicons/react/outline';
 
 import { useUser } from '@context/user';
 
 export const Sidebar = () => {
   const { user, isLoading } = useUser();
+  const [isShowing, setIsShowing] = useState(false);
+
+  useEffect(() => {
+    const element = document.getElementById('#sidebar');
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (!element?.contains(event.target as Node)) {
+        setIsShowing(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside, true);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside, true);
+    };
+  }, []);
 
   return (
-    <nav className='sticky top-0 z-20 flex flex-col items-center justify-center w-56 h-screen transition-all border-r border-gray-900 dark:border-gray-50'>
-      <div className='flex items-center h-1/4'>
-        <Logo className='text-3xl' />
-      </div>
+    <div id='sidebar'>
+      {!isShowing && (
+        <button
+          onClick={() => setIsShowing(!isShowing)}
+          className='absolute top-4 left-4 md:hidden'
+        >
+          <MenuIcon className='z-40 h-8 cursor-pointer' />
+        </button>
+      )}
+      <nav
+        className={` top-0 z-50 flex-col items-center justify-center md:w-56 h-screen border-r border-gray-900 md:flex dark:border-gray-50 transition dark:bg-gray-900 bg-gray-50 ${
+          isShowing
+            ? 'absolute flex translate-x-0 w-56'
+            : '-translate-x-56 md:translate-x-0 w-0'
+        }`}
+      >
+        <div className='relative flex justify-center w-full text-center h-1/4'>
+          {isShowing && (
+            <button
+              onClick={() => setIsShowing(!isShowing)}
+              className='absolute top-2 right-2 md:hidden'
+            >
+              <XIcon className='z-40 h-8 cursor-pointer' />
+            </button>
+          )}
+          <Logo className='text-3xl' />
+        </div>
 
-      <ul className='flex flex-col items-center justify-center w-full space-y-16 h-1/2'>
-        <NavItem name='Home' url='/home'>
-          <HomeIcon className='h-8' />
-        </NavItem>
-        <NavItem name='Courses' url='/courses'>
-          <MapIcon className='h-8' />
-        </NavItem>
-        <NavItem name='Instructors' url='/instructors'>
-          <LightBulbIcon className='h-8' />
-        </NavItem>
-        {isLoading ? (
-          <NavItem loading />
-        ) : user ? (
-          <NavItem name='Settings' url='/settings'>
-            <CogIcon className='h-8' />
+        <ul className='flex flex-col items-center justify-center w-full space-y-16 h-1/2'>
+          <NavItem name='Home' url='/home'>
+            <HomeIcon className='h-8' />
           </NavItem>
-        ) : (
-          <NavItem name='Login' url='/login'>
-            <LoginIcon className='h-8' />
+          <NavItem name='Courses' url='/courses'>
+            <MapIcon className='h-8' />
           </NavItem>
-        )}
-      </ul>
+          <NavItem name='Instructors' url='/instructors'>
+            <LightBulbIcon className='h-8' />
+          </NavItem>
+          {isLoading ? (
+            <NavItem loading />
+          ) : user ? (
+            <NavItem name='Settings' url='/settings'>
+              <CogIcon className='h-8' />
+            </NavItem>
+          ) : (
+            <NavItem name='Login' url='/login'>
+              <LoginIcon className='h-8' />
+            </NavItem>
+          )}
+        </ul>
 
-      <div className='flex items-center h-1/4'>
-        <UserProfile />
-      </div>
-    </nav>
+        <div className='flex items-center h-1/4'>
+          <UserProfile />
+        </div>
+      </nav>
+    </div>
   );
 };
