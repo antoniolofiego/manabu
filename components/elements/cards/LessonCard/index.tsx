e6b8784c7e012644a4d7705bc1ae42f240ec7c55
Antonio Lo Fiego <alofiego@hey.com>
2022-03-16 02:50:19 -0400

    Add tests for LessonCard

--- components/elements/cards/LessonCard/index.tsx
+++ components/elements/cards/LessonCard/index.tsx
@@ -15,14 +15,17 @@ export const LessonCard: React.FC<ILessonCardProps> = ({
     <a href={`/courses/${lesson.courseId}/${lesson.id}`}>
       <li
         className={`flex gap-4 px-4 py-2 hover:bg-gray-200 hover:text-gray-900 dark:hover:text-gray-50 dark:hover:bg-gray-700 transition border dark:border-gray-50 border-gray-900 rounded-xl h-16 cursor-pointer group ${
-          currentLessonId === lesson.id &&
-          'bg-gray-200 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-700'
+          currentLessonId === lesson.id && 'bg-gray-200 dark:bg-gray-700'
         }`}
+        data-testid={`lesson-card-${lesson.id}`}
       >
         <div className='grid items-center w-4 grid-rows-2'>
           <p className='text-sm text-center '>{lesson.lessonNumber}</p>
           {lesson.completed && (
-            <CheckCircleIcon className='h-4 text-green-700 dark:text-green-400' />
+            <CheckCircleIcon
+              className='h-4 text-green-700 dark:text-green-400'
+              data-testid='completed'
+            />
           )}
         </div>
         <h3 className='text-lg'>{lesson.name}</h3>
