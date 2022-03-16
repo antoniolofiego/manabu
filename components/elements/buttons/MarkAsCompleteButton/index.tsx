5ce4502954de02d164639af32bc1ce5e6838e047
Antonio Lo Fiego <alofiego@hey.com>
2022-03-15 22:44:06 -0400

    Create MarkAsComplete button

--- components/elements/buttons/MarkAsCompleteButton/index.tsx
+++ components/elements/buttons/MarkAsCompleteButton/index.tsx
@@ -1,3 +1,51 @@
-export const MarkAsCompleteButton = () => {
-  return <div></div>;
+import { useState } from 'react';
+
+import { ActionButton } from '../ActionButton';
+
+import { supabase } from '@utils/supabase';
+import { useUser } from '@context/user';
+
+import type { Dispatch, SetStateAction } from 'react';
+
+interface IMarkAsCompleteProps {
+  lessonId: string;
+  initialCompleted: boolean;
+}
+
+export const MarkAsCompleteButton: React.FC<IMarkAsCompleteProps> = ({
+  lessonId,
+  initialCompleted,
+}) => {
+  const [completed, setCompleted] = useState(initialCompleted);
+  const { user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleCompletion = async () => {
+    setLoading(true);
+    const { error } = await supabase
+      .from('progress')
+      .update({ completed: !initialCompleted })
+      .eq('user_id', user?.id)
+      .eq('lesson_id', lessonId);
+
+    if (error) {
+      console.error(error);
+    }
+
+    setCompleted((prevCompleted) => !prevCompleted);
+    setLoading(() => false);
+  };
+
+  return (
+    <ActionButton
+      clickHandler={handleCompletion}
+      loading={loading}
+      status={completed}
+      statusPrompt={{
+        trueState: 'Mark as incomplete',
+        falseState: 'Mark as completed',
+      }}
+    />
+  );
 };
