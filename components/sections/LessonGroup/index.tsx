74dafa1160bf88149f348bed1303006991eb4e03
Antonio Lo Fiego <alofiego@hey.com>
2022-03-15 21:14:23 -0400

    Fully styled LessonGroup

---
+++ components/sections/LessonGroup/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+import Link from 'next/link';
+
+import ReactPlayer from 'react-player/youtube';
+import { ArrowLeftIcon } from '@heroicons/react/outline';
+import { LessonCard, MarkAsCompleteButton } from '@components/elements';
+
+import type { LessonDetails } from '@types';
+
+interface ILessonGroupProps {
+  currentLesson: LessonDetails;
+  allLessons: LessonDetails[];
+  course: {
+    id: string;
+  };
+}
+
+export const LessonGroup: React.FC<ILessonGroupProps> = ({
+  course,
+  currentLesson,
+  allLessons,
+}) => {
+  const [completed, setCompleted] = useState(currentLesson.completed);
+
+  return (
+    <div className='w-full space-y-8 overflow-y-hidden'>
+      <Link href={`/courses/${course.id}`}>
+        <a>
+          <span className='flex items-center space-x-2 text-xl transform dark:hover:text-gray-400 hover:text-gray-700 hover:translate-x-2'>
+            <ArrowLeftIcon className='h-4' />
+            <span>Back to course page</span>
+          </span>
+        </a>
+      </Link>
+      <h3 className='text-3xl'>{`${currentLesson.lessonNumber}. ${currentLesson.name}`}</h3>
+      <div className='relative flex flex-col w-full xl:flex-row gap-x-8'>
+        <div className='flex flex-col items-end mx-auto space-y-4'>
+          <ReactPlayer
+            url={currentLesson.videoUrl}
+            playing={false}
+            controls={true}
+          />
+          <MarkAsCompleteButton
+            lessonId={currentLesson.id}
+            completed={completed}
+            setCompleted={setCompleted}
+          />
+        </div>
+        <ul className='flex flex-col flex-1 space-y-4 '>
+          {allLessons.map((lesson) => {
+            return (
+              <LessonCard
+                key={lesson.id}
+                lesson={lesson}
+                currentLessonId={currentLesson.id}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -2,6 +2,7 @@ export { CardGroup } from './CardGroup';
 export { CourseGroup } from './CourseGroup';
 export { CourseSplash } from './CourseSplash';
 export { Layout } from './Layout';
+export { LessonGroup } from './LessonGroup';
 export { ProgressGroup } from './ProgressGroup';
 export { Sidebar } from '../sections/Sidebar';
 export { SocialLogin } from './SocialLogin';
