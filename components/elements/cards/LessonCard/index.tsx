ee30b47cbbf5209be99c9297ccda92e2abe42383
Antonio Lo Fiego <alofiego@hey.com>
2022-03-15 22:44:21 -0400

    Create LessonCard component

--- components/elements/cards/LessonCard/index.tsx
+++ components/elements/cards/LessonCard/index.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import { CheckCircleIcon } from '@heroicons/react/outline';
 
 import type { LessonDetails } from '@types';
@@ -14,20 +12,21 @@ export const LessonCard: React.FC<ILessonCardProps> = ({
   currentLessonId,
 }) => {
   return (
-    <Link href={`/courses/${lesson.courseId}/${lesson.id}`} passHref>
+    <a href={`/courses/${lesson.courseId}/${lesson.id}`}>
       <li
-        className={`flex w-full gap-4 px-4 py-2 hover:bg-gray-700 dark:hover:bg-gray-200 transition border dark:border-gray-50 border-gray-900 rounded-xl h-16 ${
-          currentLessonId === lesson.id && 'bg-gray-500 hover:bg-gray-500'
+        className={`flex gap-4 px-4 py-2 hover:bg-gray-200 hover:text-gray-900 dark:hover:text-gray-50 dark:hover:bg-gray-700 transition border dark:border-gray-50 border-gray-900 rounded-xl h-16 cursor-pointer group ${
+          currentLessonId === lesson.id &&
+          'bg-gray-200 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-700'
         }`}
       >
         <div className='grid items-center w-4 grid-rows-2'>
-          <p className='text-sm text-center'>{lesson.lessonNumber}</p>
+          <p className='text-sm text-center '>{lesson.lessonNumber}</p>
           {lesson.completed && (
-            <CheckCircleIcon className='h-4 text-green-400' />
+            <CheckCircleIcon className='h-4 text-green-700 dark:text-green-400' />
           )}
         </div>
         <h3 className='text-lg'>{lesson.name}</h3>
       </li>
-    </Link>
+    </a>
   );
 };
